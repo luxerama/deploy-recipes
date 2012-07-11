5c155bd8ec14f9231a18d207c9b47cfe65bcff78
Vincent Siebert <vincent@siebert.im>
2012-07-12 00:36:34 +0100

    adding new features and fixing some bugs

--- lib/common.rb
+++ lib/common.rb
@@ -3,3 +3,15 @@ def _cset(name, *args, &block)
     set(name, *args, &block)
   end
 end
+
+def _cdefault_run_options(key, value)
+  unless default_run_options.has_key? key
+    default_run_options[key] = value
+  end
+end
+
+def _cssh_options(key, value)
+  unless ssh_options.has_key? key
+    ssh_options[key] = value
+  end
+end
--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 module Deploy
   module Recipes
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
